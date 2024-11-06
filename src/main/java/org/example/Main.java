1e1d29e701232e8925ec29a40c50024bf5912336
anzelinaharevic <g00414991@atu.ie>
2024-11-06 16:51:07 +0000

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package ...;
+
+import java.util.Scanner;
+
+public class orderSys {
+
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("please enter order ID");
+        String orderId =sc nexLine();
+        System.out.println("please enter customer name");
+        String customerName =sc nexLine();
+        System.out.println("please enter product name");
+        String productName =sc nexLine();
+        System.out.println("please enter quantity of product");
+        Integer quantity =sc nexLine();
+    }
+}
